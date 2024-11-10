92b2f2ba1b5e6fbe1a2110d29431b9880e643282
Kevin Avignon <6740474+Kavignon@users.noreply.github.com>
2024-11-10 20:24:05 +0000

    #6 Added test for module to confirm that we can launch app.

--- test/elixir_kickoff_application_test.exs
+++ test/elixir_kickoff_application_test.exs
@@ -1,8 +1,16 @@
-defmodule TemplateApplicationTest do
+# test/elixir_kickoff_application_test.exs
+defmodule ElixirKickoffApplicationTest do
   use ExUnit.Case
-  doctest TemplateApplication
 
-  test "greets the world" do
-    assert TemplateApplication.hello() == :world
+  @moduledoc """
+  Tests for the ElixirKickoff.Application module.
+  """
+
+  test "ensures the application starts correctly" do
+    # Ensure the application is started, starting it only if necessary
+    case Application.ensure_all_started(:template_application) do
+      {:ok, _apps} -> assert true
+      {:error, {:already_started, _app}} -> assert true
+    end
   end
 end
